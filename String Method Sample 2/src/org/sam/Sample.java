94ab8f1027c5a6d37a6b40d6d05ec4a0fae3d11c
Nithyaranjini <srinithya2k@gmail.com>
2023-02-27 16:54:01 +0530

    commit

---
+++ String Method Sample 2/src/org/sam/Sample.java
@@ -0,0 +1,25 @@
+package org.sam;
+
+public class Sample {
+	public static void main(String[] args) {
+		
+	
+	String s = " Nithya Padhu ";
+	System.out.println(s);
+	
+	
+	int l = s.lastIndexOf('h');
+	System.out.println(l);
+	int y = s.indexOf('a');
+	System.out.println(y);
+	String trim = s.trim();
+	System.out.println(trim);
+	
+	
+	
+	
+	
+
+	}
+
+}
