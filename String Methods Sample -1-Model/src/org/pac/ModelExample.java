7f25c1c228b0452ceaf323bf98600b1031770eaf
Nithyaranjini <srinithya2k@gmail.com>
2023-02-27 16:53:00 +0530

    commit String programs

---
+++ String Methods Sample -1-Model/src/org/pac/ModelExample.java
@@ -0,0 +1,14 @@
+package org.pac;
+
+public class ModelExample {
+	public static void main(String[] args) {
+		
+
+	String y= "java Application";
+	String y1="Java Application";
+	boolean e = y.equals(y1);
+	System.out.println(e);
+	boolean q = y.equalsIgnoreCase(y1);
+	System.out.println(q);
+
+}}
